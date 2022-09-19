c40d573adc1d251807b7fba24b8cd7c72a9f2d34
pheriksson <phi.eriksson@gmail.com>
2022-09-20 00:12:39 +0200

    added simple test for encode/decode

---
+++ network/message_test.go
@@ -0,0 +1,15 @@
+package network
+
+import "testing"
+
+func TestEncodeDecode(t *testing.T) {
+	test_msg := msg{Ping, "PAYLOAD"}
+	expected_msg := msg{Ping, "PAYLOAD"}
+	// Encode msg.
+	msg_bin, _ := encodeMsg(test_msg)
+	// Decode msg.
+	returned_msg, _ := decodeMsg(msg_bin)
+	if returned_msg != expected_msg {
+		t.Errorf("Encode and decode should be inverses")
+	}
+}
