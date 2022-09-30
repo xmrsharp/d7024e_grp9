0d76e38bac876ddfea49ef859dd756d2917f1b93
pheriksson <phi.eriksson@gmail.com>
2022-10-01 00:19:31 +0200

    forgot to add in prev commit

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,6 @@ import d7024e "D7024E_GRP9/kademlia"
 
 func main() {
 	test_node := d7024e.NewKademlia("192.168.38.111", 8888)
-	test_node.Run("192.168.38.201:8888", *d7024e.NewKademliaID("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"))
+	test_node.Run("192.168.38.201:8888", *d7024e.NewKademliaIDString("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"))
 
 }
