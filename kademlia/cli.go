4698238cb4dd67203d23af686048780efabb4b42
wilmer <wiltha-8@student.ltu.se>
2022-10-07 13:39:58 +0200

    CLI

---
+++ kademlia/cli.go
@@ -0,0 +1,31 @@
+package d7024e
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+var in *os.File = os.Stdin
+var out io.Writer = os.Stdout
+
+func cli(output io.Writer, node *Kademlia) {
+	// address := "127.0.0.1:8888" //tror det ska vara 127.0.0.(0 eller 1)
+	// con, err := net.Dial("udp", address)
+	//
+	fmt.Println("Type command: ")
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		input, _ := reader.ReadString('\n')
+		parseInput := strings.TrimSpace(input)
+
+		if parseInput == "" {
+			continue
+		} else {
+			commands := strings.Fields(parseInput)
+			Commands(output, node, commands)
+		}
+	}
+}
---
+++ kademlia/commands.go
@@ -0,0 +1,41 @@
+package d7024e
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func Commands(output io.Writer, node *Kademlia, commands []string) {
+	switch commands[0] {
+	case "put":
+		if len(commands) == 2 {
+			Put(*node, commands[1])
+		} else {
+			fmt.Println("Arg error")
+		}
+	case "get":
+		if len(commands) == 2 {
+			Get(*node, commands[1])
+		} else {
+			fmt.Println("Arg error")
+		}
+	case "exit":
+		os.Exit(0)
+	default:
+		fmt.Println(output, "invalid command")
+	}
+}
+
+func Put(node Kademlia, input string) {
+	hash := node.Store(&input)
+	fmt.Println("Hash is: ", hash)
+}
+func Get(node Kademlia, hash string) {
+	value, id := node.LookupData(hash)
+	if id == nil {
+		fmt.Println("")
+	} else {
+		fmt.Println("Value found: ", value)
+	}
+}
