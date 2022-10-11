c7def801870fcbee9b0ae3c5ce34e833aa580284
pheriksson <phi.eriksson@gmail.com>
2022-10-11 23:35:46 +0200

    post endpoint properly set

--- api/api_server.go
+++ api/api_server.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"strconv"
@@ -12,6 +13,7 @@ type APIChannel struct {
 	ApiResponseChannel chan string
 	ApiRequestMsg      string
 }
+
 type APIServer struct {
 	channelNode chan<- APIChannel
 	server      *http.Server
@@ -26,28 +28,21 @@ func NewServer(addr string, port int, ch chan APIChannel) *APIServer {
 	httpServer := http.Server{Addr: addr + ":" + strconv.Itoa(port)}
 	handler := http.NewServeMux()
 	httpServer.Handler = handler
-	// Have to create instance first
 	apiServer := APIServer{ch, &httpServer}
 
 	// Endpoints
 	handler.HandleFunc("/objects", apiServer.postObject)
 	handler.HandleFunc("/objects/", apiServer.getObject)
+
 	return &apiServer
 }
 
 func (as *APIServer) Listen() {
-	log.Println("API SERVING ON:", as.server.Addr)
+	log.Println("REST API SERVING ON:", as.server.Addr)
 	err := as.server.ListenAndServe()
 	if err != nil {
 		log.Println(err)
 	}
-}
-
-// TODO Fix correct encoding of payloads of /objects & /objects/{hash}
-// TODO Fix status codes of response POST
-
-type postObjectResponse struct {
-	Location string `json:location`
 }
 
 type getObjectResponse struct {
@@ -56,49 +51,58 @@ type getObjectResponse struct {
 
 // POST /objects - create object in http body
 func (as *APIServer) postObject(w http.ResponseWriter, r *http.Request) {
-	// value := r.Body
-	if r.Method != "POST" {
-		log.Println("DID NOT RECIEVE POST")
+	// Read value to save.
+	storeValue, _ := io.ReadAll(r.Body)
+
+	if r.Method != "POST" || len(storeValue) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+
 	nodeChannelMsg := APIChannel{ApiResponseChannel: make(chan string),
-		ApiRequestMsg: "INCOMING POST REQUEST WITH BODY"}
+		ApiRequestMsg: ""}
 
 	// Write msg to node.
 	as.channelNode <- nodeChannelMsg
 	// Await response in wrapped channel
-	nodeResp := <-nodeChannelMsg.ApiResponseChannel
+	nodeRsp := <-nodeChannelMsg.ApiResponseChannel
+	// TODO Remove when making sure calls to create node and get node are working.
+	log.Println("RECIEVED RESP:", nodeRsp)
 
-	log.Println("RECIEVED RESP:", nodeResp)
-	w.Header().Set("Content-Type", "application/json")
-	respBody := postObjectResponse{Location: "/objects/" + "THIS_IS_A_TEST_VALUE"}
-	json.NewEncoder(w).Encode(respBody)
+	// Return response to caller
+	w.Header().Add("Location", "/objects/"+nodeRsp)
+	w.WriteHeader(http.StatusCreated)
+	w.Write(storeValue)
 }
 
 // GET /objects/{hash} - simply return with body of the key (basically hash is key).
 // RESPOND WITH 200 - "hash: value"
 // NOTE /objects/{hash} : GET value of hash if exist.
 func (as *APIServer) getObject(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		log.Println("DID NOT RECIEVE GET")
+	// NOTE This is will always work as long as /objects will be taken by other endpoint
+	// That being said, do not try this at home
+	hashString := strings.Split(r.URL.String(), "/")[2]
+	hash := []byte(hashString)
+
+	// NOTE Hardcoded 20 byte value for byte id size.
+	if r.Method != "GET" || len(hash) != 20 {
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	// NOTE This is will always work as /objects will be taken by other endpoint
-	// That being said, do not try this at home
-	temp := strings.Split(r.URL.String(), "/")
-	hash := temp[2]
-
 	nodeChannelMsg := APIChannel{ApiResponseChannel: make(chan string),
-		ApiRequestMsg: "INCOMING GET REQUEST WITH HASH:" + hash}
+		ApiRequestMsg: "INCOMING GET REQUEST WITH HASH:"}
 
 	// Write msg to node.
 	as.channelNode <- nodeChannelMsg
 	// Await response in wrapped channel
-	nodeResp := <-nodeChannelMsg.ApiResponseChannel
+	nodeRsp := <-nodeChannelMsg.ApiResponseChannel
 
-	log.Println("RECIEVED RESP:", nodeResp)
+	// TODO: Make sure storevalue and findnode are valid.
+	log.Println("RECIEVED RESP:", nodeRsp)
 
+	// Return response to caller.
 	w.Header().Set("Content-Type", "application/json")
-	payload_test := getObjectResponse{Value: "THIS_IS_A_TEST_VALUE"}
-	json.NewEncoder(w).Encode(payload_test)
+	body := getObjectResponse{Value: "THIS_IS_A_TEST_VALUE"}
+	json.NewEncoder(w).Encode(body)
+
 }
