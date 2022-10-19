b0454c732d4c31662331c1be5a8910a0bd7a22bd
david <davidwass90@gmail.com>
2022-10-19 14:27:23 +0200

    Over 50% cov complete
    Will push for 80%

--- kademlia/kademlia_test.go
+++ kademlia/kademlia_test.go
@@ -2,8 +2,8 @@ package kademlia
 
 import (
 	"fmt"
-
 	"testing"
+	"time"
 )
 
 const (
@@ -19,5 +19,59 @@ func TestNewKademlia(t *testing.T) {
 	BootLoaderId := NewKademliaIDString(BOOT_LOADER_STRING)
 	node := NewKademlia(nodeIp, PORT_KAD, PORT_API, BootLoaderId)
 	fmt.Printf("Node: %T\n", node)
+}
+
+func TestNodeLookup(t *testing.T) {
+	fmt.Printf("Test nodelookup")
+	nodeIp := "127.0.0.1"
+	BootLoaderId := NewKademliaIDString(BOOT_LOADER_STRING)
+	node := NewKademlia(nodeIp, PORT_KAD, PORT_API, BootLoaderId)
+
+	contact := NewContact(NewKademliaIDString("FFFFFFFF00000000000000000000000000000000"), "localhost:8000")
+	//rt := NewRoutingTable(contact)
+	<-time.After(2 * time.Second)
+	/*Create kademlia network for bootstrap node*/
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF0000000000000000000000000000000F"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF000000000000000000000000000000F0"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF00000000000000000000000000000F00"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF0000000000000000000000000000F000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF000000000000000000000000000F0000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF00000000000000000000000000F00000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF0000000000000000000000000F000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF000000000000000000000000F0000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF00000000000000000000000F00000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF0000000000000000000000F000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF000000000000000000000F0000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF00000000000000000000F00000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF0000000000000000000F000000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF000000000000000000F0000000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF00000000000000000F00000000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF0000000000000000F000000000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF000000000000000F0000000000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF00000000000000F00000000000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF0000000000000F000000000000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF000000000000F0000000000000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF00000000000F00000000000000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF0000000000F000000000000000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF000000000F0000000000000000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF00000000F00000000000000000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF0000000F000000000000000000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF000000F0000000000000000000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF00000F00000000000000000000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF000F0000000000000000000000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF00F00000000000000000000000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFF0F000000000000000000000000000000"), "localhost:8080"))
+	node.routingTable.AddContact(NewContact(NewKademliaIDString("FFFFFFFFF0000000000000000000000000000000"), "localhost:8080"))
+	node.routingTable.AddContact(contact)
+
+	node.NodeLookup(contact.ID)
+	node.genCheckBuckets()
+
+	node.LookupData(*NewRandomKademliaID())
+
+	val := "testarstore"
+	key := NewKademliaID(&val)
+	node.StoreValue(val)
+	node.LookupData(key)
 
 }
